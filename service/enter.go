29facceecbf676e0cc37aeab5b15980e080c2381
冯明凡 <1264857660@qq.com>
2022-08-13 15:43:47 +0800

    course-admin init

---
+++ service/enter.go
@@ -0,0 +1,15 @@
+package service
+
+import (
+	"github.com/flipped-aurora/gin-vue-admin/server/service/example"
+	"github.com/flipped-aurora/gin-vue-admin/server/service/home"
+	"github.com/flipped-aurora/gin-vue-admin/server/service/system"
+)
+
+type ServiceGroup struct {
+	SystemServiceGroup  system.ServiceGroup
+	ExampleServiceGroup example.ServiceGroup
+	HomeServiceGroup    home.ServiceGroup
+}
+
+var ServiceGroupApp = new(ServiceGroup)
